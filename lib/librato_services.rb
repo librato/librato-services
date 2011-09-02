b94d8f22b2cf0b7ef69f5d750d8e3b80546e960b
Mike Heffner <mikeh@fesnel.com>
2011-09-02 10:00:56 -0400

    Add version method.

--- lib/librato_services.rb
+++ lib/librato_services.rb
@@ -1,1 +1,12 @@
-require 'librato_services/service'
+
+$:.unshift File.join(File.dirname(__FILE__), 'librato-services')
+
+require 'service'
+
+module Librato
+  module Services
+    def self.version
+      File.read(File.join(File.dirname(__FILE__), '../VERSION')).chomp
+    end
+  end
+end
