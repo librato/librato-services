a829110f77124d49fea4b10ef06fd76dcb5db0ef
Mike Heffner <mikeh@fesnel.com>
2011-10-12 12:25:25 -0400

    Add a custom webhook service allowing POSTs to any custom URL.

---
+++ services/webhook.rb
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+require 'uri'
+require 'cgi'
+
+class Service::Webhook < Service
+  def receive_validate(errors = {})
+    if settings[:url].to_s.empty?
+      errors[:url] = "Is required"
+      return false
+    end
+
+    uri = URI.parse(settings[:url]) rescue nil
+    unless uri
+      errors[:url] = "Is not a valid URL"
+      return false
+    end
+
+    return true
+  end
+
+  def receive_alert
+    raise_config_error unless receive_validate({})
+
+    uri = URI.parse(settings[:url])
+
+    result = {
+      :alert => payload['alert'],
+      :metric => payload['metric'],
+      :measurement => payload['measurement'],
+      :trigger_time => payload['trigger_time']
+    }
+
+    # Faraday doesn't unescape user and password
+    if uri.userinfo
+      http.basic_auth *uri.userinfo.split(":").map{|x| CGI.unescape(x)}
+    end
+
+    url = "%s://%s:%d%s" % [uri.scheme, uri.host, uri.port, uri.request_uri]
+
+    http_post url, {:payload => Yajl::Encoder.encode(result)}
+  rescue Faraday::Error::ConnectionFailed
+    raise_error "Connection refused — invalid URL."
+  end
+end
