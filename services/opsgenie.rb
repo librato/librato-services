7f3cc4418dfb0533d98b389508f0f34bb472cd65
halitokumus <halit.okumus@ifountain.com>
2014-04-10 20:57:12 +0300

    Update opsgenie.rb
    Hi; I've been working on a new implementation of OpsGenie's Librato integration. We need to commit  this service as quickly as possible in order to make the integration work properly on our side. 
    
    Thanks for your help;
    Halit

--- services/opsgenie.rb
+++ services/opsgenie.rb
@@ -15,31 +15,14 @@ class Service::OpsGenie < Service
 		if settings[:recipients].to_s.empty?
 			settings[:recipients] = "all"
 		end
-				
-                message = "[Librato Metrics] Metric #{payload[:metric][:name]} value: #{payload[:measurement][:value]} has triggered an alert!"
-                
-                details = {
-                        :"Alert Id" => payload[:alert][:id],
-                        :Metric => payload[:metric][:name],
-                        :"Measurement Value"=> payload[:measurement][:value],
-                        :"Triggered At" => Time.at(payload[:trigger_time]).utc,
-                        :"Measurement Source" => payload[:measurement][:source],
-                        :"Metric Link" => metric_link(payload[:metric][:type],payload[:metric][:name])
-                }
-
-                details[:"Alert Name"] = payload[:alert][:name] if payload[:alert][:name]
 
                 params = {
-                  :customerKey => settings[:customer_key],
-                  :recipients => settings[:recipients],
-                  :alias => payload[:metric][:name],
-                  :message => message,
-                  :source => "Librato",
-                  :details => details
-                }
+         	  :apiKey => settings[:customer_key],
+         	  :payload => payload,
+         	  :recipients => settings[:recipients]
+        	}
 
-                url = "https://api.opsgenie.com/v1/json/alert"
+                url = "http://api.opsgenie.com/v1/json/librato"
                 http_post url, params, 'Content-Type' => 'application/json'
         end
 end
-
