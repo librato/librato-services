2571796e2945d26db82318132df1bb806608669e
Mike Heffner <mikeh@fesnel.com>
2014-07-26 12:29:03 -0400

    Raise exception on failure to deliver hipchat message.

--- services/hipchat.rb
+++ services/hipchat.rb
@@ -90,7 +90,8 @@ class Service::Hipchat < Service
   end
 
   def send_message(msg, format)
-    hipchat.rooms_message(settings[:room_id], settings[:from], msg,
-                         settings[:notify].to_i, 'yellow', format)
+    r = hipchat.rooms_message(settings[:room_id], settings[:from], msg,
+                              settings[:notify].to_i, 'yellow', format)
+    raise Exception.new(r) unless r.success?
   end
 end
