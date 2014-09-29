e655de23c5911eed06db9bd30ba982cba898311f
Ray Jenkins <rjenkins@aceevo.com>
2014-09-29 11:00:16 -0500

    adding new test to verify timeout works on service.receive

---
+++ test/timeout_service_test.rb
@@ -0,0 +1,23 @@
+class TimeoutServiceTest < Librato::Services::TestCase
+
+  class TimeoutService < Service
+    def receive_alert_clear
+    end
+    def sample_payload
+      {}
+    end
+    def receive_timeout_request
+      sleep 30
+    end
+  end
+
+  def test_verify_timeout_error
+    event = :timeout_request
+    settings = {}
+    payload = { }
+    begin
+      assert TimeoutService.receive(event, settings, payload)
+    rescue Timeout::Error => e
+    end
+  end
+end
