ef8a0ef596f37123290465632e048804cdee649f
Collin Van Dyck <collin@librato.com>
2014-10-15 11:03:41 -0700

    Revert "Disable opsgenie clearing support for now"
    This reverts commit 73ab8bbfee57311d88e5f8586bf00dc75a8c14e8.

--- test/opsgenie_test.rb
+++ test/opsgenie_test.rb
@@ -8,6 +8,51 @@ class OpsGenieTest < Librato::Services::TestCase
     @stubs = Faraday::Adapter::Test::Stubs.new
   end
 
+  def test_v2_alert_clear_normal
+    payload = new_alert_payload.dup
+    payload[:clear] = "normal"
+    svc = service(:alert, @settings, payload)
+    @stubs.post @stub_url do |env|
+      assert_equal("Alert Some alert name has cleared at 1970-05-23 14:32:03 UTC", env[:body][:message])
+      [200, {}, '']
+    end
+    svc.receive_alert_clear
+  end
+
+  def test_v2_alert_clear_unknown
+    payload = new_alert_payload.dup
+    payload[:clear] = "dont know"
+    svc = service(:alert, @settings, payload)
+    @stubs.post @stub_url do |env|
+      assert_equal("Alert Some alert name has cleared at 1970-05-23 14:32:03 UTC", env[:body][:message])
+      [200, {}, '']
+    end
+    svc.receive_alert_clear
+  end
+
+  def test_v2_alert_clear_manual
+    payload = new_alert_payload.dup
+    payload[:clear] = "manual"
+    svc = service(:alert, @settings, payload)
+    @stubs.post @stub_url do |env|
+      assert_equal("Alert Some alert name was manually cleared at 1970-05-23 14:32:03 UTC", env[:body][:message])
+      [200, {}, '']
+    end
+    svc.receive_alert_clear
+  end
+
+  def test_v2_alert_clear_auto
+    payload = new_alert_payload.dup
+    payload[:clear] = "auto"
+    svc = service(:alert, @settings, payload)
+    @stubs.post @stub_url do |env|
+      assert_equal("Alert Some alert name was automatically cleared at 1970-05-23 14:32:03 UTC", env[:body][:message])
+      [200, {}, '']
+    end
+    svc.receive_alert_clear
+  end
+
+
   def service(*args)
     super Service::OpsGenie, *args
   end
