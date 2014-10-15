1770c4502dc1651ca05cfc5dd85249e3d92ca8d6
Collin Van Dyck <collin@librato.com>
2014-10-15 11:29:22 -0700

    Try and fix error

--- services/opsgenie.rb
+++ services/opsgenie.rb
@@ -36,10 +36,10 @@ class Service::OpsGenie < Service
 
     if payload[:alert][:version] == 2
       message = "Alert #{payload[:alert][:name]} has triggered!"
-      payload = payload.dup
-      payload.delete(:auth)
-      payload.delete(:settings)
-      do_post(payload[:alert][:name], message, payload)
+      details = payload.dup
+      details.delete(:auth)
+      details.delete(:settings)
+      do_post(payload[:alert][:name], message, details)
       return
     end
 
