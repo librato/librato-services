2dfdceac2b727d7be1432189198d5880a7c02eda
obfuscurity <jason@dixongroup.net>
2014-11-20 13:36:59 -0500

    validate hipchat notify param

--- services/hipchat.rb
+++ services/hipchat.rb
@@ -30,6 +30,11 @@ class Service::Hipchat < Service
       errors[:from] = "string has invalid characters" if /^[\w\s\.\-\_]+$/.match(settings[:from]).nil?
     end
 
+    # check that nofify is boolean
+    if !settings[:from].nil?
+      errors[:notify] = "must be a 0 or 1" unless ['0', '1'].include?(settings[:notify])
+    end
+
     errors.empty?
   end
 
