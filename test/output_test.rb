76f1f2bc71595fbf491abbb3d36a24448ce6934a
Jason Derrett <jason@librato.com>
2014-12-15 11:32:57 -0600

    Escape underscores in alert_name when rendering HTML format

--- test/output_test.rb
+++ test/output_test.rb
@@ -232,4 +232,34 @@ Runbook: http://example.com/
 EOF
     assert_equal(expected, output.markdown)
   end
+
+  # Escape underscores in alert name when rendering html
+  def test_escape_underscores_in_alert_name
+    payload = {
+        alert: {id: 123, name: "Some_alert_name", version: 2, runbook_url: "http://example.com/"},
+        settings: {},
+        service_type: "campfire",
+        event_type: "alert",
+        trigger_time: 12321123,
+        conditions: [{type: "above", threshold: 10, id: 1}],
+        violations: {
+            "foo.bar" => [{
+                              metric: "metric.name", value: 100, recorded_at: 1389391083,
+                              condition_violated: 1
+                          }]
+        }
+    }
+    output = Librato::Services::Output.new(payload)
+    expected = <<EOF
+# Alert Some\\_alert\\_name has triggered!
+
+Link: https://metrics.librato.com/alerts/123
+
+Source `foo.bar`:
+* metric `metric.name` was above threshold 10 with value 100 recorded at Fri, Jan 10 2014 at 21:58:03 UTC
+
+Runbook: http://example.com/
+EOF
+    assert_equal(expected, output.generate_markdown(is_html=true))
+  end
 end
