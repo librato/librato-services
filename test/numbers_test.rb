002455dbdc227cc79895bf789c5fc037a2f9789d
Collin Van Dyck <collinvandyck@gmail.com>
2015-10-02 16:11:34 -0700

    Uncomment test

--- test/numbers_test.rb
+++ test/numbers_test.rb
@@ -8,10 +8,10 @@ class NumbersTest < Test::Unit::TestCase
     Librato::Services::Numbers.format_for_threshold(threshold,number,tolerance)
   end
 
-  #def test_does_not_change
-    #assert format(10, 10.53) == 10.53
-    #assert format(10.53, 10.53) == 10.53
-  #end
+  def test_does_not_change
+    assert format(10, 10.53) == 10.53
+    assert format(10.53, 10.53) == 10.53
+  end
 
   def test_changes
     assert_equal 10.5312, format(10.53, 10.5312345)
